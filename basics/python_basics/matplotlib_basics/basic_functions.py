c00933126513eabc356a2dacc932e0dce88688ec
Divyansh Kejriwal <diyansh.shok@gmail.com>
2025-11-26 13:16:04 +0530

    update

--- basics/python_basics/matplotlib_basics/basic_functions.py
+++ basics/python_basics/matplotlib_basics/basic_functions.py
@@ -1,7 +1,19 @@
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-"""Basic functionality"""
+"""BASIC GRAPH"""
 
-a = [1,2,3,4,4,5,6,7]
-b= mpl.matplotlib_fname()
-print(b)
+x = [0,2,3,4]
+y = [0,5,6,7]
+a = plt.plot(x,y,label = "random_data")                ## -----> a simple line graph in terms of x and y axes and labels the line as random_data
+
+plt.title("Basic Graph")                ## -----> Shows a title at the top of the graph
+plt.xlabel("X-axis") ; plt.ylabel("Y-axis") ## -----> Adds label on x and y axis
+
+plt.xticks([1,2,3,4,5,6,7,8,9,10])          ## -----> The unit of X-axis changes to the give values in this case integer
+plt.yticks([1,2,3,4,5,6,7,8,9,10])          ## -----> The unit of Y-axis changes to the give values in this case integer
+
+plt.legend()                                ## -----> puts a legend on the graph
+
+plt.show()                                  ## -----> Used to display a graph
